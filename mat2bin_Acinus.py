9eeafb47c153b076654bb21f1238a0b6136a9918
jrugis <j.rugis@auckland.ac.nz>
2022-06-06 21:38:58 +1200

    combine acinus nodes in single file

--- mat2bin_Acinus.py
+++ mat2bin_Acinus.py
@@ -11,35 +11,43 @@ import struct
 ##################################################################
 
 # ********* HARD CODED *************
-nacinii = 14  # number of acinii
+ncells = 14  # number of acinii
 # **********************************
 
 ##################################################################
 # main program
 ##################################################################
 
-for n in range(nacinii):
-  f = open("_4Unity_acinus_c" + str(n+1) + ".bin", 'wb') # create binary file for Unity
+f = open("_4Unity_acinus.bin", 'wb') # create binary file for Unity
+
+nnodes = 0 
+for n in range(ncells):
   ml = sc.loadmat("acinus_data/a" + str(n+1) + ".mat") # get acinus data (Matlab)
   #print('keys:', ml.keys())
   #print(ml['time_samples'].shape)
   #print(ml['xc'].shape)
-  nnodes = ml['xp'].shape[0]  # number of nodes
-  ntsteps = ml['xc'].shape[1] # number of timesteps
-  print("cell_" + str(n+1) + "   " + str(nnodes) + " nodes, " + str(ntsteps)+ " timesteps" )
+  nnodes += ml['xp'].shape[0] # number of nodes
+print("nodes: " + str(nnodes))
+f.write(struct.pack('i', nnodes))
 
-  f.write(struct.pack('i', nnodes))
-  for p in ml['xp']:
-    f.write(struct.pack('fff', p[0], p[1], p[2]))    # node coordinates
+ntsteps = ml['xc'].shape[1] # number of timesteps (same for all cells)
+print("timesteps: " + str(ntsteps))
+f.write(struct.pack('i', ntsteps))
 
-  f.write(struct.pack('i', ntsteps))
-  for t in ml['time_samples'][0]:
-    f.write(struct.pack('f', t))    # time at step
+for n in range(ncells):                            # for each cells...
+  ml = sc.loadmat("acinus_data/a" + str(n+1) + ".mat") # get acinus data (Matlab)
+  for p in ml['xp']:                               #   for each node...
+    f.write(struct.pack('fff', p[0], p[1], p[2]))  #     write node coordinates
 
-  for nc in ml['xc']:              # for each node...
-    for c in nc:                   #   for each timestep.. 
-      f.write(struct.pack('f', c)) #      write calcium value
+for t in ml['time_samples'][0]: # for each timestep...
+  f.write(struct.pack('f', t))  #   write time at step (same for all cells)
 
-  f.close()    # close binary file
+for n in range(ncells):            # for each cell...
+  ml = sc.loadmat("acinus_data/a" + str(n+1) + ".mat") # get acinus data (Matlab)
+  for nc in ml['xc']:              #   for each node...
+    for c in nc:                   #      for each timestep.. 
+      f.write(struct.pack('f', c)) #         write calcium value
+
+f.close()    # close binary file
 
 
