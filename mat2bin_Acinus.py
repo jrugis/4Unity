85ca25df69c816ff0a322143406e17dd1dc5c639
jrugis <j.rugis@auckland.ac.nz>
2022-06-09 15:07:57 +1200

    transpose calcium data array

--- mat2bin_Acinus.py
+++ mat2bin_Acinus.py
@@ -21,11 +21,11 @@ ncells = 14  # number of acinii
 f = open("_4Unity_acinus.bin", 'wb') # create binary file for Unity
 
 nnodes = 0 
-for n in range(ncells):
-  ml = sc.loadmat("acinus_data/a" + str(n+1) + ".mat") # get acinus data (Matlab)
+for c in range(ncells):
+  ml = sc.loadmat("acinus_data/a" + str(c+1) + ".mat") # get acinus data (Matlab)
   #print('keys:', ml.keys())
   #print(ml['time_samples'].shape)
-  #print(ml['xc'].shape)
+  print(ml['xc'].shape)
   nnodes += ml['xp'].shape[0] # number of nodes
 print("nodes: " + str(nnodes))
 f.write(struct.pack('i', nnodes))
@@ -34,19 +34,19 @@ ntsteps = ml['xc'].shape[1] # number of timesteps (same for all cells)
 print("timesteps: " + str(ntsteps))
 f.write(struct.pack('i', ntsteps))
 
-for n in range(ncells):                            # for each cells...
-  ml = sc.loadmat("acinus_data/a" + str(n+1) + ".mat") # get acinus data (Matlab)
+for c in range(ncells):                                # for each cells...
+  ml = sc.loadmat("acinus_data/a" + str(c+1) + ".mat") # get acinus data (Matlab)
   for p in ml['xp']:                               #   for each node...
     f.write(struct.pack('fff', p[0], p[1], p[2]))  #     write node coordinates
 
 for t in ml['time_samples'][0]: # for each timestep...
   f.write(struct.pack('f', t))  #   write time at step (same for all cells)
 
-for n in range(ncells):            # for each cell...
-  ml = sc.loadmat("acinus_data/a" + str(n+1) + ".mat") # get acinus data (Matlab)
-  for nc in ml['xc']:              #   for each node...
-    for c in nc:                   #      for each timestep.. 
-      f.write(struct.pack('f', c)) #         write calcium value
+for c in range(ncells):            # for each cell...
+  ml = sc.loadmat("acinus_data/a" + str(c+1) + ".mat") # get acinus data (Matlab)
+  for ts in np.transpose(ml['xc']):  #   for each timestep...
+    for n in ts:                     #      for each node.. 
+      f.write(struct.pack('f', n))   #         write calcium value
 
 f.close()    # close binary file
 
